63d89470d7a6196b957b12790a0daf36a7d16127
x229466 <victor.r.matos@accenture.com>
2020-05-18 11:15:49 -0300

    feat(master): Fist commit :tada:

---
+++ src/main/java/com/gitinterface/GithubInterface/GithubInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.gitinterface.GithubInterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubInterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitinterface/GithubInterface/GithubInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitinterface.GithubInterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
